0f382c900076679b6b0b59220aa8b07406c390fc
Lukasz Mentel <lmmentel@gmail.com>
2017-10-17 19:00:31 +0200

    updated the module and added disk_mask

--- xrdtools.py
+++ xrdtools.py
@@ -8,10 +8,39 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
-def create_matrix(filename, colnum, rank, sep=':', savecsv=False):
+def parse_data(filename, colnum, sep=':', savecsv=False):
+    '''
+    Parse data from `filename` to pandas DataFrame
 
+    Args:
+        filename : str
+            name of the data file
+        colnum : int
+            number of the column with the data to plot
+        sep : str
+            value separator in the file
+        savecsv : bool
+            a flag to mark if a csv file should be save with parsed data
+
+    Returns:
+        matrix : array_like
+
+    '''
+
+    # read the data from file
     df = pd.read_csv(filename, sep=sep, header=None, comment='#')
-    matrix = df.iloc[:, [colnum]].values.reshape(rank, rank)
+
+    # extract indices from the first column in the file
+    ind = df.iloc[:, 0].str.split('_', expand=True)
+    ind.rename(columns={2: 'ind_x', 3: 'ind_y'}, inplace=True)
+    ind.loc[:, 'ind_x'] = ind['ind_x'].astype(int)
+    ind.loc[:, 'ind_y'] = ind['ind_y'].astype(int)
+
+    # merged the indices with the main df
+    df = df.join(ind[['ind_x', 'ind_y']])
+
+    # pivot the dataframe to
+    matrix = df.pivot(index='ind_x', columns='ind_y', values=colnum)
 
     if savecsv:
         mname = os.path.splitext(os.path.basename(filename))[0] + '_col-{0:d}'.format(colnum) + '.csv'
@@ -19,17 +48,21 @@ def create_matrix(filename, colnum, rank, sep=':', savecsv=False):
 
     return matrix
 
-def heatmap(matrix, size, cmap='viridis', show_colorbar=True):
+
+def heatmap(matrix, size=10, cmap='viridis', show_colorbar=True):
     '''
+    Create a heatmap form a matrix of data
+
     Args:
         matrix : array_like
             Matrix to be colormapped
         size : int
             Size of the plot in inches
+
     '''
 
     plt.figure(figsize=(size, size))
-    plt.pcolor(m, cmap=cmap)
+    plt.pcolor(matrix, cmap=cmap)
 
     ax = plt.gca()
     ax.set_aspect('equal')
@@ -43,12 +76,35 @@ def heatmap(matrix, size, cmap='viridis', show_colorbar=True):
     plt.show()
 
 
-if __name__ == '__main__':
+def disk_mask(array, x0, y0, radius):
+    '''
+    Create a circular mask for a rectangular array
+
+    Args:
+        array : array_like
+            array with the data
+        x0, y0 : int
+            coordinates of the origin of the circle
+        radius : int
+            radius of the circle
+
+    Returns:
+        mask : array_like
+
+    '''
+
+    xdim, ydim = array.shape
+    y, x = np.ogrid[-x0:xdim - x0, -y0:ydim - y0]
+    mask = x * x + y * y < radius**2
+    return mask
+
+
+def cli():
+    'CLI interface'
 
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', help='name of the data file')
     parser.add_argument('colnum', type=int, help='column number (starting from 0)')
-    parser.add_argument('rank', type=int, help='rank of the matrix to be formed')
     parser.add_argument('--cmap', default='viridis', help='matplotlib colormap name, default is "viridis"')
     parser.add_argument('--sep', default=':', help='separator for columns in data file, default is ":"')
     parser.add_argument('--plotsize', type=int, default=10, help='size of the plot in inches')
@@ -57,6 +113,10 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    m = create_matrix(args.filename, args.colnum, args.rank, sep=args.sep, savecsv=args.savecsv)
+    m = parse_data(args.filename, args.colnum, sep=args.sep, savecsv=args.savecsv)
     heatmap(m, args.plotsize, cmap=args.cmap, show_colorbar=args.hide_cbar)
 
+
+if __name__ == '__main__':
+
+    cli()
